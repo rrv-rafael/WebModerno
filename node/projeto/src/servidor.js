9aff2d9bc97d5238176bd7b1d2cfe506a5647aa9
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-05-15 21:14:48 -0300

    Novos arquivos do estudo de express.

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,42 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/produtos', (request, response, next) => {
-    response.send({ nome: 'Notebook', preco: 123.45 }) // O método send() irá converter este objeto para JSON
+    response.send(bancoDeDados.getProdutos()) // O método send() irá converter este objeto para JSON
+})
+
+app.get('/produtos/:id', (request, response) => {
+    response.send(bancoDeDados.getProduto(request.params.id))
+})
+
+app.post('/produtos', (request, response, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: request.body.nome,
+        preco: request.body.preco
+    })
+
+    response.send(produto) // Isso converte para um JSON
+})
+
+app.put('/produtos/:id', (request, response, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: request.params.id,
+        nome: request.body.nome,
+        preco: request.body.preco
+    })
+
+    response.send(produto) // Isso converte para um JSON
+})
+
+app.delete('/produtos/:id', (request, response, next) => {
+    const produto = bancoDeDados.excluirProduto(request.params.id)
+
+    response.send(produto) // Isso converte para um JSON
 })
 
 app.listen(porta, () => {
