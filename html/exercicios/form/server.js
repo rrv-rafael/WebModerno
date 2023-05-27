1ecd4d733900177d7c343d42b178871c8652e223
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-05-27 14:49:00 -0300

    Capítulo sobre HTML finalizado.

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/usuarios', (request, response) => {
     console.log(request.body)
-    response.send('<h1>Parabéns</h1>')
+    response.send('<h1>Parabéns usuário incluído!</h1>')
+})
+
+app.post('/usuarios/:id', (request, response) => {
+    console.log(request.params.id)
+    console.log(request.body)
+    response.send('<h1>Parabéns usuário alterado!</h1>')
 })
 
 app.listen(3003)
