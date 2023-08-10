bf1f2ad5068ceb6fae934433c497eae867bf176c
Rafael Rodrigues Vitor <rrv.rafael@outlook.com>
2023-08-09 22:50:52 -0300

    Criação do header.

--- bootstrap/projeto-galeria/webpack.config.js
+++ bootstrap/projeto-galeria/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     mode: modoDev ? 'development' : 'production',
     entry: './src/index.js',
     devServer: {
-        contentBase: './build',
+        static: './build',
         port: 9000,
     },
     optimization: {
@@ -41,8 +41,7 @@ module.exports = {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader', // interpreta @import, url()...
+                'css-loader',
                 'sass-loader',
             ]
         }, {
